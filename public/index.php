d3265ef59af5f12976bc0732587cb016f1c42721
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-10 17:36:40 +0100

    Update

--- public/index.php
+++ public/index.php
@@ -24,29 +24,19 @@ require_once '../routes/web.php';
 
 <body>
     <div class="bg-red-500">
-
         <div class="container py-4 flex flex-wrap items-center justify-between">
-
             <?php $quanta->renderComponent('navbarComponent'); ?>
-
         </div>
-
     </div>
 
     <div class="container py-4 flex flex-wrap items-center justify-between">
-
         <?php $quanta->processRouting(); ?>
-
     </div>
 
     <div class="bg-red-500">
-
         <div class="container py-4 flex flex-wrap items-center justify-between">
-
             <?php $quanta->renderComponent('footerComponent'); ?>
-
         </div>
-
     </div>
 
     <link rel="stylesheet" href="<?php echo $quanta->loadAsset('public/js/flowbite.min.js'); ?>">
