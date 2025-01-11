7904bffd13d39cada08b3a9d298b4d5c6c589763
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-11 17:50:34 +0100

    Update

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ require_once '../routes/web.php';
 
 require_once("../packages/DiscordWidgetModule/DiscordWidgetModule.php");
 
-$discordWidget = new DiscordWidgetModule('1311801047475294271');
+$discordWidget = new DiscordWidgetModule('discordwidget', '1311801047475294271');
 $discordWidget->load($quanta);
 
 $quanta->renderComponent("layout");
