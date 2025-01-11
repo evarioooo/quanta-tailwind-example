c839d04ac666712d975a572e712a65f8dbbcbd11
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-11 18:11:37 +0100

    Update

--- packages/DiscordWidgetModule/DiscordWidgetModule.php
+++ packages/DiscordWidgetModule/DiscordWidgetModule.php
@@ -11,12 +11,30 @@ class DiscordWidgetModule extends Module
     private ?Quanta $quanta;
     protected $guildId;
 
+    /**
+     * The constructor function initializes the object with the provided module ID and guild ID.
+     * 
+     * @param moduleId The `moduleId` parameter is used to identify the module to which the object
+     * belongs. It is passed to the constructor of the class to initialize the object with the
+     * specified module ID.
+     * @param guildId The `guildId` parameter in the constructor function is used to initialize the
+     * `guildId` property of the class instance. It allows you to set the `guildId` value when creating
+     * an object of this class.
+     */
     public function __construct($moduleId, $guildId)
     {
         parent::__construct($moduleId);
         $this->guildId = $guildId;
     }
 
+    /**
+     * The function `loadServer` makes a GET request to retrieve JSON data from a Discord server's
+     * widget endpoint.
+     * 
+     * @return The `loadServer()` function makes a GET request to the Discord API to retrieve
+     * information about a specific guild (server) using the guild ID. It then decodes the JSON
+     * response and returns it as an associative array.
+     */
     public function loadServer()
     {
         $client = new Client();
@@ -26,11 +44,27 @@ class DiscordWidgetModule extends Module
         return json_decode($response->getBody(), true);
     }
 
+    /**
+     * The dispose function sets the quanta property to null.
+     * 
+     * @param quanta The `dispose` function in the code snippet takes a parameter named ``.
+     * However, in the function body, it sets the property `->quanta` to `null`, not the parameter
+     * ``. This means that the function is not using the parameter passed to it.
+     */
     public function dispose($quanta)
     {
         $this->quanta = null;
     }
 
+    /**
+     * The function "load" sets the "quanta" property and adds a Discord widget component to the
+     * component handler.
+     * 
+     * @param quanta The `load` function takes a parameter named ``. This parameter is being
+     * assigned to the property `->quanta` within the function. It seems like `` is an
+     * object or an instance of a class that has a `componentHandler` property. The function then
+     * creates
+     */
     public function load($quanta)
     {
         $this->quanta = $quanta;
--- public/index.php
+++ public/index.php
@@ -3,21 +3,31 @@
 error_reporting(E_ALL);
 
 require '../vendor/autoload.php';
+
+/* layout component */
 require_once '../components/layout.php';
+
+/* pages components */
 require_once '../components/home.php';
 require_once '../components/contact.php';
 
+/* partials components */
 require_once '../components/header.php';
 require_once '../components/meta.php';
 require_once '../components/navbar.php';
 require_once '../components/footer.php';
 
+/* load routes */
 require_once '../routes/web.php';
 
 require_once("../packages/DiscordWidgetModule/DiscordWidgetModule.php");
 
+/* add discord widget modules */
 $discordModule = new DiscordWidgetModule('discordwidget', '1311801047475294271');
 $quanta->addModule($discordModule);
+
+/* add all modules */
 $quanta->loadModules();
 
+/* render layout component */
 $quanta->renderComponent("layout");
